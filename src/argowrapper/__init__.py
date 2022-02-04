bed78292a2de9ba851979df8384d8b2a73577153
UchicagoZchen138 <zchen138@uchicago.edu>
2022-02-04 13:35:26 -0600

    feat add get workflow for user api

---
+++ src/argowrapper/__init__.py
@@ -0,0 +1,3 @@
+from cdislogging import get_logger
+
+logger = get_logger("requestor", log_level="debug")
--- src/argowrapper/routes/workflow.py
+++ src/argowrapper/routes/workflow.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from ..engine import ArgoEngine
+from argowrapper.engine import ArgoEngine
 
 router = APIRouter()
 argo_engine = ArgoEngine()
@@ -8,14 +8,15 @@ argo_engine = ArgoEngine()
 
 @router.get("/test")
 def test():
+    """route to test that the argo-workflow is correctly running"""
     return {"message": "test"}
 
 
 # submit argo workflow
 @router.post("/submit")
 def submit_workflow():
+    """route to submit workflow"""
     # authenticate()
-    print(f"we are hitting the submit endpoint")
     message = argo_engine.submit_workflow({})
     return {"message": message}
 
@@ -23,6 +24,7 @@ def submit_workflow():
 # get status
 @router.get("/status/{workflow_name}")
 def get_workflow_status(workflow_name: str):
+    """returns current status of a workflow"""
     # is_authorized = authenticate()
     # if not is_authorized:
     #    raise HTTPException(status_code=404, detail="user not authorized access to workflow status")
@@ -33,6 +35,15 @@ def get_workflow_status(workflow_name: str):
 # cancel workflow
 @router.post("/cancel/{workflow_name}")
 def cancel_workflow(workflow_name: str):
+    """cancels a currently running workflow"""
     # authenticate()
     message = argo_engine.cancel_workflow(workflow_name)
+    return {"message": message}
+
+
+# get workflows
+@router.get("/workflows/{user_name}")
+def get_workflows(user_name: str):
+    """returns the list of workflows the user has ran"""
+    message = argo_engine.get_workfows_for_user(user_name)
     return {"message": message}
