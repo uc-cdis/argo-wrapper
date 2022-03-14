2587b359a813b5070bd8e16554caf5fbf3483392
UchicagoZchen138 <86243966+UchicagoZchen138@users.noreply.github.com>
2022-03-14 13:26:08 -0500

    doc auto generate swagger (#10)

---
+++ doc/generate_swagger_yaml.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import yaml
+
+from argowrapper.app import get_app
+
+
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+schema = get_app().openapi()
+path = os.path.join(CURRENT_DIR, "openapi.yaml")
+yaml.Dumper.ignore_aliases = lambda *args: True
+with open(path, "w+") as f:
+    yaml.dump(schema, f, default_flow_style=False)
+print(f"Saved docs at {path}")
