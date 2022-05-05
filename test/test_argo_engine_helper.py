f349973aa48aceaa6f8f1dac729a69bcdb72c505
UchicagoZchen138 <86243966+UchicagoZchen138@users.noreply.github.com>
2022-05-05 14:09:47 -0500

    add unit tests for argo_engine_helper.py to get coverage above 80% (#38)

--- test/test_argo_engine_helper.py
+++ test/test_argo_engine_helper.py
@@ -8,6 +8,8 @@ import yaml
 import pytest
 import re
 
+EXAMPLE_AUTH_HEADER = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb250ZXh0Ijp7InVzZXIiOnsibmFtZSI6InRlc3QgdXNlciJ9fX0.FJUBv2hAhUG--Rhosxy6iLNKitvUTSGxDl8ZXIWBUdE"  # pragma: allowlist secret
+
 
 def _convert_to_label(special_character_match: str) -> str:
     if (match := special_character_match.group()) :
@@ -44,6 +46,7 @@ workflow_yaml = yaml.safe_load(stream)
 
 
 def test_argo_engine_helper_add_parameters_to_gwas_workflow():
+    """tests that parameters are added correctly from request body to gwas workflow"""
     parameters = {
         "pheno_csv_key": "test_replace_value",
         "n_pcs": 100,
@@ -63,23 +66,54 @@ def test_argo_engine_helper_add_parameters_to_gwas_workflow():
 
 
 def test_argo_engine_helper_add_name_to_workflow():
+    """tests generated workflow name added to gwas workflow"""
     workflow_name = "wf_123"
     argo_engine_helper.generate_workflow_name = mock.MagicMock(
         return_value=workflow_name
     )
     argo_engine_helper.add_name_to_workflow(workflow_yaml)
 
-    assert "generateName" not in workflow_yaml["metadata"]
     assert workflow_yaml["metadata"]["name"] == workflow_name
 
 
-def test_argo_engine_helper_add_scaling_groups():
+def test_argo_engine_helper_prod_add_scaling_groups():
+    """tests scaling groups in prod are added"""
     config = {"scaling_groups": {"test_user": (group := "group_1")}}
 
     argo_engine_helper._get_argo_config_dict = mock.MagicMock(return_value=config)
     argo_engine_helper.add_scaling_groups("test_user", workflow_yaml)
 
     assert workflow_yaml["spec"]["nodeSelector"]["role"] == group
+
+
+def test_argo_engine_helper_qa_add_scaling_groups():
+    """tests scaling groups in qa do not exist"""
+    config = {"qa_scaling_groups": {"test_user": (group := "group_1")}}
+
+    argo_engine_helper._get_argo_config_dict = mock.MagicMock(return_value=config)
+    argo_engine_helper.add_scaling_groups("test_user", workflow_yaml)
+
+    assert workflow_yaml["spec"].get("nodeSelector") is None
+
+
+def test_add_gen3user_label_and_annotation():
+    """tests user label and annotations are added to workflow"""
+    username_label_pair = UsernameLabelPair("abc123", "user-abc123")
+    argo_engine_helper.add_gen3user_label_and_annotation(
+        username_label_pair.username, workflow_yaml
+    )
+
+    assert (
+        workflow_yaml["spec"]["podMetadata"]["labels"]["gen3username"]
+        == username_label_pair.label
+    )
+    assert (
+        workflow_yaml["metadata"]["labels"]["gen3username"] == username_label_pair.label
+    )
+    assert (
+        workflow_yaml["spec"]["podMetadata"]["annotations"]["gen3username"]
+        == username_label_pair.username
+    )
 
 
 UsernameLabelPair = namedtuple("UsernameLabelPair", "username label")
@@ -98,3 +132,83 @@ def test_convert_username_label_to_gen3username(username, label):
 @pytest.mark.parametrize("username,label", user_label_data)
 def test_convert_gen3username_to_label(username, label):
     assert convert_username_label_to_gen3username(label) == username
+
+
+WorkflowStatusData = namedtuple("WorkflowStatusData", "parsed_phase shutdown phase")
+phase_shutdown_data = [
+    WorkflowStatusData("Canceling", "Terminate", "Running"),
+    WorkflowStatusData("Canceled", "Terminate", "Failed"),
+    WorkflowStatusData("Running", "Running", "Running"),
+]
+
+
+@pytest.mark.parametrize("parsed_phase,shutdown,phase", phase_shutdown_data)
+def test_parse_status(parsed_phase, shutdown, phase):
+    """tests that status workflow get correct phase based on workflow shutdown and phase"""
+    wf_status_dict = {
+        "metadata": {"name": "test_wf"},
+        "spec": {
+            "arguments": "test_args",
+            "shutdown": shutdown,
+        },
+        "status": {
+            "phase": phase,
+            "progress": "1/5",
+        },
+    }
+
+    parsed_status = argo_engine_helper.parse_status(wf_status_dict)
+    assert parsed_status.get("phase") == parsed_phase
+
+
+def test_get_username_from_token():
+    """tests context["user"]["name"] can be parsed from example auth header"""
+    assert (
+        argo_engine_helper.get_username_from_token(EXAMPLE_AUTH_HEADER) == "test user"
+    )
+
+
+def test_add_cohort_middleware_request():
+    """tests cohort middleware parameters are added to gwas workflow"""
+    config = {"environment": "default"}
+    argo_engine_helper._get_argo_config_dict = mock.MagicMock(return_value=config)
+
+    request_body = {
+        "covariates": ["ID_2000006886", "ID_2000000324"],
+        "outcome": "ID_2000006885",
+        "source_id": 4,
+        "cohort_definition_id": 70,
+    }
+
+    argo_engine_helper.add_cohort_middleware_request(request_body, workflow_yaml)
+    for dict in workflow_yaml["spec"]["arguments"]["parameters"]:
+        if dict.get("name") == "cohort_middleware_url":
+            assert (
+                dict["value"]
+                == "http://cohort-middleware-service.default/cohort-data/by-source-id/4/by-cohort-definition-id/70"
+            )
+        if dict.get("name") == "cohort_middleware_body":
+            assert (
+                dict["value"]
+                == '{"PrefixedConceptIds": ["ID_2000006886", "ID_2000000324", "ID_2000006885"]}'
+            )
+
+
+def test_setup_workspace_token_service():
+    """tests workspace token service is working properly for indexd"""
+    config = {"environment": "default"}
+    argo_engine_helper._get_argo_config_dict = mock.MagicMock(return_value=config)
+
+    with mock.patch(
+        "argowrapper.engine.argo_engine.argo_engine_helper.requests.get"
+    ) as mock_get:
+        mock_get.return_value = {"status_code": 400}
+
+        assert (
+            argo_engine_helper.setup_workspace_token_service(EXAMPLE_AUTH_HEADER)
+            == False
+        )
+
+        mock_get.return_value = {"status_code": 403}
+
+        argo_engine_helper.setup_workspace_token_service(EXAMPLE_AUTH_HEADER) == True
