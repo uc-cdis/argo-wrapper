bc09c656bfece28005205aaf0646b4c24d9e5e7f
UchicagoZchen138 <86243966+UchicagoZchen138@users.noreply.github.com>
2022-06-08 15:19:03 -0500

    docs update swagger (#49)

--- docs/swagger/generate_swagger_yaml.py
+++ docs/swagger/generate_swagger_yaml.py
@@ -7,7 +7,7 @@ from argowrapper.app import get_app
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 schema = get_app().openapi()
-path = os.path.join(CURRENT_DIR, "openapi.yaml")
+path = os.path.join(CURRENT_DIR, "swagger.yaml")
 yaml.Dumper.ignore_aliases = lambda *args: True
 with open(path, "w+") as f:
     yaml.dump(schema, f, default_flow_style=False)
