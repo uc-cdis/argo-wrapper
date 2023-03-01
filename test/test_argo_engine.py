b5475bd5b536c767ff6b2b3e5b79aa24aed8a2cd
qiongl-CTDS <qiongl@uchicago.edu>
2023-03-01 12:24:45 -0600

    fixed a typo

--- test/test_argo_engine.py
+++ test/test_argo_engine.py
@@ -5,7 +5,7 @@ import unittest.mock as mock
 import pytest
 import yaml
 
-from argo_workflows.exception import NotFoundException
+from argo_workflows.exceptions import NotFoundException
 from argowrapper import argo_workflows_templates
 from argowrapper.constants import *
 from argowrapper.engine.argo_engine import *
