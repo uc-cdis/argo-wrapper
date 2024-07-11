35c1a390bd3750fa91d5a5b01aaf13c8575b6c50
wangfan860 <fan1@uchicago.edu>
2024-07-11 10:38:50 -0500

    test get_workflow_logs

--- src/argowrapper/engine/argo_engine.py
+++ src/argowrapper/engine/argo_engine.py
@@ -517,7 +517,7 @@ class ArgoEngine:
             )
             raise exception
 
-    def get_workflow_logs(self, uid: str):
+    def get_workflow_logs(self, workflow_name: str, uid: str):
         """
         Gets the workflow errors from failed workflow
 
