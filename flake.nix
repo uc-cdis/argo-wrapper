8189d9c58e0dd997e25c43e4309fa3ea80527d5a
Andrew Prokhorenkov <aprokh@uchicago.edu>
2024-10-16 04:58:18 -0500

    feat: add nix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Python application packaged using poetry2nix";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
+
+  outputs = { self, nixpkgs, poetry2nix }:
+    let
+      system = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      pkgs = nixpkgs.legacyPackages.${system};
+      # create a custom "mkPoetryApplication" API function that under the hood uses
+      # the packages and versions (python3, poetry etc.) from our pinned nixpkgs above:
+      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      myPythonApp = mkPoetryApplication { projectDir = ./.; };
+    in
+    {
+      apps.${system}.default = {
+        type = "app";
+        # replace <script> with the name in the [tool.poetry.scripts] section of your pyproject.toml
+        program = "${myPythonApp}/bin/<script>";
+      };
+    };
+}
